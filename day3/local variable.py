600f441472a2fcc25fe341b466eb3c454aa7a526
chyang26qw <47452491+chyang26qw@users.noreply.github.com>
2019-07-28 23:16:57 +0800

    Update local variable.py

--- day3/local variable.py
+++ day3/local variable.py
@@ -2,12 +2,12 @@ lover = "Melody"
 
 def change_name(name):
     global lover
-    lover = "LOVE KATHY"
+    lover = "LOVE mmmm"
     print("before change",name,lover)
     name = "Kathy" #這個函數就是這個變量的作用域
     print("after change",name)
 
-    name = "Hemry"
+    name = "Henry"
     change_name(name)
     print(name)
     print(lover)
